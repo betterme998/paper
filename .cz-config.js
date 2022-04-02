62542ff48a1dc95e244cf83bdbfca86a6ddb8cda
betterme998 <1248715031@qq.com>
2022-04-02 20:23:00 +0800

    feat: 测试

--- .cz-config.js
+++ .cz-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // 可选类型
-  type: [
+  types: [
     {value: 'feat',name: 'feat:     新功能'},
     {value: 'fix',name: 'fix:     修复'},
     {value: 'docs',name: 'docs:     文档变更'},
